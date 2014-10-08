9db25f5a1899885ffea32f03c39e5a8faa093e2b
minihessel <arntzn@gmail.com>
2014-10-08 12:44:42 +0200

    etter formatering

--- src/Controller/MainFXMLController.java
+++ src/Controller/MainFXMLController.java
@@ -2,7 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- */
+ *///
 package Controller;
 
 import combined.SQL_manager;
